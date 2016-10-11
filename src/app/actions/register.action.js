1a572837e98f6bc21a450f49be1d3aa622a7fb23
ahoopen <auke@frontmen.nl>
2016-10-11 21:46:38 +0200

    Added register actions

---
+++ src/app/actions/register.action.js
@@ -0,0 +1,37 @@
+import * as RegisterEnum from '../constants/register.constants';
+
+export const UserRegisterRequest = () => ({
+    type: RegisterEnum.USER_REGISTER_REQUEST
+});
+
+export const UserRegisterSuccess = (data) => ({
+    type: RegisterEnum.USER_REGISTER_COMPLETED,
+    payload: data
+});
+
+export const UserRegisterFailed = (error) => ({
+    type: RegisterEnum.USER_REGISTER_FAILED,
+    payload: {
+        status: error
+    }
+});
+
+export const UserRegister = (data) => {
+    const {username, password, email} = data;
+
+    return (dispatch) => {
+        dispatch(UserRegisterRequest());
+
+        return fetch('http://127.0.0.1:1337/api/user/register', {
+                method: 'post',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({username, email, password})
+            })
+            .then(response => response.json())
+            .then(json => dispatch(UserRegisterSuccess(json)))
+            .catch(UserRegisterFailed());
+    }
+};
---
+++ src/app/constants/register.constants.js
@@ -0,0 +1,3 @@
+export const USER_REGISTER_REQUEST = 'UserRegisterRequest';
+export const USER_REGISTER_FAILED = 'UserRegisterFailed';
+export const USER_REGISTER_COMPLETED = 'UserRegisterCompleted';
