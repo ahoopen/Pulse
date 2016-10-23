72bace8ea50d5f210b3f594d7e73f43ebb046a46
ahoopen <auke@frontmen.nl>
2016-10-23 22:06:55 +0200

    Added user regiser actions

--- src/app/actions/register.action.js
+++ src/app/actions/register.action.js
@@ -16,20 +16,58 @@ export const UserRegisterFailed = (error) => ({
     }
 });
 
+export const UserActivated = (data) => ({
+    type: RegisterEnum.USER_REGISTER_ACTIVATED,
+    payload: data
+});
+
+export const UserActivationFailed = (data) => ({
+    type: RegisterEnum.USER_REGISTER_ACTIVATE_FAILED,
+    payload: data
+});
+
+export const UserActivate = (activateCode) => {
+    return (dispatch) => {
+
+        return fetch('http://127.0.0.1:1337/api/user/activate', {
+            method: 'post',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({activateCode})
+        })
+        .then(response => response.json())
+        .then((response) => {
+            if (response.success) {
+                dispatch(UserActivated(response));
+            } else {
+                dispatch(UserActivationFailed(response));
+            }
+        })
+        .catch(json => dispatch(UserActivationFailed(json)))
+    }
+};
+
+
 export const UserRegister = (data) => {
     return (dispatch) => {
         dispatch(UserRegisterRequest());
 
         return fetch('http://127.0.0.1:1337/api/user/register', {
-                method: 'post',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            })
-            .then(response => response.json())
-            .then(json => dispatch(UserRegisterSuccess(json)))
-            .catch(UserRegisterFailed());
+            method: 'post',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then((response) => {
+            response.success ? dispatch(UserRegisterSuccess(response))
+                : dispatch(UserRegisterFailed(response))
+
+        })
+        .catch(UserRegisterFailed());
     }
 };
