cd6eb1c4ac8976bd25f65dff68c5822c48991019
ahoopen <auke@frontmen.nl>
2016-10-23 22:05:14 +0200

    removed unnessary components on dashboard page

--- src/app/page/dashboard.js
+++ src/app/page/dashboard.js
@@ -1,20 +1,11 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
-import {AppCanvas, AppBar, Styles, FlatButton, Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui';
-
 class Dashboard extends Component {
 
     render() {
         return (
             <div>
-                <AppBar title={`Support Ticket Manager`}
-                        iconElementRight={ <FlatButton label="Debug" /> }/>
-                <Toolbar>
-                    <ToolbarGroup key={1}>
-                        <ToolbarTitle text="henk"/>
-                    </ToolbarGroup>
-                </Toolbar>
                 <div id="page">
                     dit is het dashboard
                 </div>
